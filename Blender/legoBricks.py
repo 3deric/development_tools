d2ad8e8136dfe1162e035ac325c22e3589c88c83
Eric Schubert <42551187+3deric@users.noreply.github.com>
2022-10-14 20:50:52 +0200

    Added Lego colour palette
    Added lego colour palette, switched from posterize to quantize function, added blur before the resizing of the image

--- Blender/legoBricks.py
+++ Blender/legoBricks.py
@@ -3,20 +3,88 @@
 #Data is stored in a csv file.
 #The blocks are defined as bitmask values in a 4 by 4 raster.
 
-from PIL import Image, ImageOps 
+from PIL import Image, ImageOps, ImageFilter
 import csv
 import math
 import random
 
 width = 64
-posterize = 2
 
 #Predefined blocks
 blocks = [1,3,7,15,17,51,255,273,4369,13107,65535]
 
+#Lego Color Palette
+palette = [ 
+    21,21,21,
+    66,66,62,
+    100,103,101,
+    160,161,159,
+    239,239,238,
+    119,119,121,
+    135,141,143,
+    244,244,244,
+    206,206,208,
+    150,117,180,
+    188,166,208,
+    76,47,146,
+    118,114,181,
+    0,57,94,
+    0,108,183,
+    120,191,234,
+    0,153,212,
+    72,158,206,
+    103,130,151,
+    0,163,218,
+    0,190,211,
+    24,158,159,
+    91,193,191,
+    91,193,191,
+    193,228,218,
+    111,148,122,
+    0,74,45,
+    0,146,71,
+    0,175,77,
+    154,202,60,
+    204,225,151,
+    130,131,83,
+    0,168,79,
+    150,199,83,
+    251,171,24,
+    255,205,3,
+    247,209,18,
+    255,245,121,
+    195,151,55,
+    210,161,42,
+    249,108,98,
+    240,87,41,
+    245,125,32,
+    245,136,48,
+    59,24,13,
+    105,46,20,
+    166,83,34,
+    174,116,70,
+    222,139,95,
+    252,195,158,
+    148,126,95,
+    151,137,108,
+    221,196,142,
+    127,19,27,
+    221,26,33,
+    229,30,38,
+    181,28,125,
+    232,80,156,
+    233,93,162,
+    246,173,205
+    ]
+    
+
+#Palette Image for quantize function
+paletteImage = Image.new('P', (64, 1))
+paletteImage.putpalette(palette)
+
 #opening a image file and saving it as a pixel array
 #im = Image.open("Drive:/imagefile.png") #windows format
-im = Image.open("/home/user/imagefile.jpg") #linux format
+im = Image.open("/home/eric/IMG_4193.jpg") #linux format
 
 #getting source width, height and aspect
 w, h = im.size
@@ -29,11 +97,18 @@ print(height)
 print(a)
 
 #resizing 
+im = im.filter(ImageFilter.BLUR)
 im = im.resize((width,height), resample =0)
 
 #reducing image colors
-im = ImageOps.posterize(im, posterize) 
-im.save("/home/user/imagefileresized.jpg")
+#im = ImageOps.posterize(im, posterize) 
+
+im = im.quantize(0,0,0,paletteImage) 
+im = im.convert("RGB")
+im.save("/home/eric/imagefileresized.png")
+
+#Flipping the image horizontal
+im = ImageOps.mirror(im)
 
 #readim the image into a list
 imVals=list(im.getdata())
@@ -123,7 +198,7 @@ for i in image:
 
 #output file as csv
 #only saves coordinates with a valid bitmask value
-with open('/home/user/imagefile.csv', mode='w') as image_file: #linux
+with open('/home/eric/imagefile.csv', mode='w') as image_file: #linux
 #with open('Drive:/imagefile.csv', mode='wb') as image_file: #windows
         image_writer = csv.writer(image_file, delimiter=',', quotechar ='',escapechar=' ', quoting=csv.QUOTE_NONE)
         y = 0 
